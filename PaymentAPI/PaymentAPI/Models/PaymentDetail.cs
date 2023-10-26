4cc4cf0d517b5b8782c8611230264e69f4ae4ca6
Herbert Owusu <77298594+herbs61@users.noreply.github.com>
2023-10-26 14:34:28 +0000

    First commit

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string CardOwnerName { get; set; } = "";
+        [Column(TypeName = "varchar(16)")]
+        public string CardNumber { get; set; } = "";
+        //mm/yy
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; } = "";
+        [Column(TypeName = "varchar(3)")]
+        public string SecurityCode {  get; set; } = "";
+    }
+
+}
